54c7dadb85fcb1933b05f3a734a4103e27409511
Wenshuai Hou <wenhoujx@gmail.com>
2013-10-11 11:03:25 -0400

    .

--- learn_sql_thehardway/ex06/ex13.sql
+++ learn_sql_thehardway/ex06/ex13.sql
@@ -4,3 +4,9 @@
 alter table person add column dead integer; 
 alter table person add column phone_number integer; 
 alter table person add column salary float; 
+
+alter table person add column dob datetime; 
+alter table pet add column dob datetime; 
+alter table person_pet add column purchased_on  datetime; 
+
+alter table pet add column parent integer; 
